c6d81ef38c9f6850186c47a972b317b4151674aa
semonemo00 <rsemo@naver.com>
2025-09-16 10:05:01 +0900

    javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+// 상수(변할 수 없는 값)
+const username = "세모네모" 
+console.log("🚀 ~ username:", username)
+
+// 변수(변할 수 있는 값)
+let usermbti = "ENTJ"
+console.log(usermbti);
+
+usermbti = "ISTJ"
+console.log("변경된 usermbti:", usermbti)
+
+username = "김진아"
+console.log("변경된 userㅜ믇:", usermbti)
