ac621e97cbc022a8a9e80c5a00944f9c52d85bb9
semonemo00 <rsemo@naver.com>
2025-09-16 15:22:15 +0900

    객체

---
+++ array.js
@@ -0,0 +1,7 @@
+// 객체: key-value기 한 쌍으로 구성된 정보들의 묶음
+const person = {
+  name: "김진아", isMale:false, mbti:"ENTJ"
+}
+console.log("🚀 ~ person:", person)
+console.log("name: ", person["name"])
+console.log("남성여부: ", person.isMale)
